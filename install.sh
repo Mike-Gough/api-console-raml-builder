2200abeedb3ae0028f02bff6200b4b8029a47da1
Michael Gough <goughm@cdc-mule-dev.comsuper.int>
2019-01-18 13:20:57 +1100

    Woking example using artifactory

--- install.sh
+++ install.sh
@@ -12,26 +12,11 @@ fi
 USERNAME="$HTTP_PROXY_USERNAME"
 PASSWORD="$HTTP_PROXY_PASSWORD"
 HERE=$(dirname .)
-
-echo "setting npm config"
-./binaries/node-v10.15.0-linux/bin/npm config set strict-ssl false
-./binaries/node-v10.15.0-linux/bin/npm config set cache $HERE/npm-cache
-./binaries/node-v10.15.0-linux/bin/npm config set proxy "http://$USERNAME:$PASSWORD@cswebproxy.comsuper.int:80/"
-./binaries/node-v10.15.0-linux/bin/npm config set https-proxy "http://$USERNAME:$PASSWORD@cswebproxy.comsuper.int:80/"
-
-echo "setting git local config"
-git config url."https://".insteadOf git://
-git config url."https://github.com/".insteadOf git@github.com/
-git config http.sslVerify false
-git config http.proxy "http://$USERNAME:$PASSWORD@cswebproxy.comsuper.int:80/"
-git config https.proxy "http://$USERNAME:$PASSWORD@cswebproxy.comsuper.int:80/"
-
-#echo "setting git global config"
-#git config --global url."https://".insteadOf git://
-#git config --global url."https://github.com/".insteadOf git@github.com/
-#git config --global http.sslVerify false
-#git config --global http.proxy "http://$USERNAME:$PASSWORD@cswebproxy.comsuper.int:80/"
-#git config --global https.proxy "http://$USERNAME:$PASSWORD@cswebproxy.comsuper.int:80/"
+#OLD_PATH=$PATH
+#export PATH="/tmp/api-console-raml-builder/binaries/node-v10.15.0-linux/bin:$OLD_PATH"
 
 echo "installing npm dependancies"
-./binaries/node-v10.15.0-linux/bin/npm install
+npm config set registry https://cdc-aphmv-dev.comsuper.int:8443/artifactory/api/npm/virtual-npm/
+npm install -d
+
+#export PATH="$OLD_PATH"
